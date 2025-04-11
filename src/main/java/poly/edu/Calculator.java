c2f5f1497e11f27ea444344da407fffb3863461f
diepmthuan <thuandmps38008@gmail.com>
2025-04-12 01:01:56 +0700

    firt

---
+++ src/main/java/poly/edu/Calculator.java
@@ -0,0 +1,15 @@
+package poly.edu;
+
+
+public class Calculator implements calculatorInterface {
+
+	@Override
+	public Integer sum(Integer a, Integer b) {
+		return 0;
+	}
+	@Override
+	public Integer sub(Integer a, Integer b) {
+		return 0;
+	}
+
+}
